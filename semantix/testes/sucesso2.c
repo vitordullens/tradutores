8ef8eb43464eff9d8e29bb013c14e168220f0b28
Vitor Dullens <vitordullens2.0@gmail.com>
2021-04-14 23:56:52 -0300

    sucesso2

--- semantix/testes/sucesso2.c
+++ semantix/testes/sucesso2.c
@@ -1,3 +1,23 @@
-int main(){
-  return 1.2;
+float x(){
+  return 1;
+}
+
+int y(float a, float b){
+  return a+b;
+}
+
+int main() {
+  int a;
+  float b;
+  set s;
+  add(1 in s);
+
+  remove(1 in s);
+
+  if(s == s) {
+    for(a = 1; a < 1.2; a = a + b) {
+      a = b;
+    }
+  }
+  return 1;
 }
